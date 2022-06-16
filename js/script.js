efdace01b3c6c690ac2617aaad1c7c0362c1450d
igorsvr <buldozerom@gmail.com>
2022-06-16 13:59:58 +0200

    Bootstrap JQuery

---
+++ js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carousel-button").click(function(){
+        if ($("#carousel-button").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carousel-button").children("span").removeClass('fa-pause');
+            $("#carousel-button").children("span").addClass('fa-play');
+        }
+        else if ($("#carousel-button").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carousel-button").children("span").removeClass('fa-play');
+            $("#carousel-button").children("span").addClass('fa-pause');
+        }
+    });
+
+      $('#loginButton').click(function() {
+        $('#loginModal').modal('show');
+      });
+      $('#reserveButton').click(function() {
+        $('#reservationModal').modal('show');
+      });
+
+    });
+
+    /* $(document).ready(function(){
+        $("#mycarousel").carousel( { interval: 2000 } );
+                      $("#carousel-pause").click(function(){
+            $("#mycarousel").carousel('pause');
+        });
+                    $("#carousel-play").click(function(){
+            $("#mycarousel").carousel('cycle');
+        });
+    });
+ 
+    $(document).ready(function(){
+        $("#reserveTableNow").click(function(){
+            $("#reserveTableForm").modal('show');
+        });
+        
+        $("#loginButton").click(function(){
+            $("#loginModal").modal('show');
+        });
+    }); */
